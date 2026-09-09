c5bd5980613f0fa15bbb974cde09916702212d7b
anujkumaryadav1 <kumaryadavanuj7172@gmail.com>
2026-09-09 21:58:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (14.29%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x == 0 || x == 1) return x ;
+         long long target = x;
+        long long  s =0 ;
+        long long e = x;
+        long long  mid = s + (e-s)/2;
+        long long  ans = -1;
+        while (s <= e){
+            long long  square = mid*mid ; 
+            if( square == target){
+                return mid ;
+
+            }else if( square > target ){
+                e = mid -1;
+            }else{
+                ans =mid;
+                s = mid + 1;
+            }
+            mid = s+ (e-s)/2;
+        }
+        return ans;
+    }
+};
